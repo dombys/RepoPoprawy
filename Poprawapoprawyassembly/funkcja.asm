f71d4afd4b0a9bc35208ac6b43a0978386005eda
Dominik Bystryk <dominikbys@gmail.com>
2024-05-14 16:13:56 +0200

    Kolejne zmiany w .asm
    Zmiana nazw funkcji w etykiety

--- Poprawapoprawyassembly/funkcja.asm
+++ Poprawapoprawyassembly/funkcja.asm
@@ -3,15 +3,14 @@ section .text
 
 dodaj:
   	enter 0,0
-readyoffsets:
 	mov esi, [ebp + 8]
 	mov edi, [ebp + 12]
 	mov ecx, [ebp + 16]
-add:
+.add:
 	add esi, ecx ;ustawienie wskaźnika tablicy 1
 	add edi, ecx ;ustawienie wskaźnika tablicy 2
 	mov eax, [esi]
 	add eax, [edi]
-done:
+.done:
  	leave
   	ret
